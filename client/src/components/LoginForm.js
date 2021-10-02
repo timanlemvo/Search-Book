f50836fc169d174e0783d6bfa7630e4363f74f79
timanlemvo <timanlemvo@gmail.com>
2021-10-01 20:36:37 -0700

    update

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,90 +1,91 @@
-import React, { useState, useEffect } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
-import { useMutation } from '@apollo/react-hooks';
-import { LOGIN_USER } from '../utils/mutations';
+import React, { useState } from 'react';
 import Auth from '../utils/auth';
+import { Form, Button, Alert } from 'react-bootstrap';
+import { LOGIN_USER } from '../utils/mutations';
+import { useMutation } from '@apollo/react-hooks';
 
 const LoginForm = () => {
-const [userFormData, setUserFormData] = useState({ email: '', password: '' });
-const [validated] = useState(false);
-const [showAlert, setShowAlert] = useState(false);
-const [login, { error }] = useMutation(LOGIN_USER);
+    const [login, {error} ] = useMutation(LOGIN_USER);
+    const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+    const [validated] = useState(false);
+    const [showAlert, setShowAlert] = useState(false);
 
-  useEffect(() => {
-    if (error) {
-      setShowAlert(true);
-    } else {
-      setShowAlert(false);
-    }
-  }, [error]);
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setUserFormData({
+            ...userFormData,
+            [name]: value
+        });
+    };
 
-const handleInputChange = (event) => {
-const { name, value } = event.target;
-    setUserFormData({ ...userFormData, [name]: value });
-  };
+    const handleFormSubmit = async event => {
+        event.preventDefault();
 
-const handleFormSubmit = async (event) => {
-    event.preventDefault();
+        // check if form has everything (as per react-bootstrap docs)
+        // const form = event.currentTarget;
+        // if (form.checkValidity() === false) {
+        //   event.preventDefault();
+        //   event.stopPropagation();
+        // }
 
-    // check if form has everything (as per react-bootstrap docs)
-const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+        try {
+            const { data } = await login({
+                variables: {...userFormData}
+            });
+            Auth.login(data.login.token);
+        } catch (e) {
+            console.error(e);
+            setShowAlert(true);
+        }
 
-    try {
-const { data } = await login({variables: { ...userFormData },
-      });
+        setUserFormData({
+            username: '',
+            email: '',
+            password: '',
+        });
+    };
 
-      console.log(data);
-      Auth.login(data.login.token);
-    } catch (e) {
-      console.error(e);
-    }
-      setUserFormData({
-      email: '',
-      password: '',
-    });
-  };
+    return (
+        <>
+            <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+                <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+                    Something went wrong with your login credentials!
+                </Alert>
+                <Form.Group>
+                    <Form.Label htmlFor='email'>Email</Form.Label>
+                    <Form.Control
+                        type='text'
+                        placeholder='Your email'
+                        name='email'
+                        onChange={handleInputChange}
+                        value={userFormData.email}
+                        required
+                    />
+                    <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
+                </Form.Group>
 
-  return (
-    <>
-      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
-          Incorrect Login Info!
-        </Alert>
-        <Form.Group>
-          <Form.Label htmlFor='email'>Email</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Your email'
-            name='email'
-            onChange={handleInputChange}
-            value={userFormData.email}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Enter your e-mail</Form.Control.Feedback>
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Label htmlFor='password'>Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='Your password'
-            name='password'
-            onChange={handleInputChange}
-            value={userFormData.password}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Enter your password</Form.Control.Feedback>
-        </Form.Group>
-        <Button disabled={!(userFormData.email && userFormData.password)} type='submit' variant='success'>
-          Submit
-        </Button>
-      </Form>
-    </>
-  );
+                <Form.Group>
+                    <Form.Label htmlFor='password'>Password</Form.Label>
+                    <Form.Control
+                        type='password'
+                        placeholder='Your password'
+                        name='password'
+                        onChange={handleInputChange}
+                        value={userFormData.password}
+                        required
+                    />
+                    <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
+                </Form.Group>
+                <Button
+                    disabled={!(userFormData.email && userFormData.password)}
+                    type='submit'
+                    variant='success'>
+                    Submit
+                </Button>
+                {error && <div>Login failed</div>}
+            </Form>
+        </>
+    );
 };
 
 export default LoginForm;
--- server/server.js
+++ server/server.js
@@ -1,37 +1,44 @@
-const path = require('path');
 const express = require('express');
+const path = require('path');
 const db = require('./config/connection');
-const { ApolloServer } = require('apollo-server-express');
 // const routes = require('./routes');
-const { typeDefs, resolvers } = require('./schemas');
+const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
+const { resolvers, typeDefs } = require('./schemas');
+const { User } = require('./models');
 
-const PORT = process.env.PORT || 3001;
+
+
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware
-});
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+})
 
-server.applyMiddleware({ app });
+server.start().then(() => {
+  server.applyMiddleware({ app });
+})
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'))
+// })
 
-db.once('open', () => {
-    app.listen(PORT, () => {
-        console.log(`Now listening on localhost:${PORT}`);
-        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    });
+// app.use(routes);
+
+db.once('open', ()  => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+});
 });
